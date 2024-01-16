f0f18a2b5c4d32cd977465d545fa0950119fe5f8
newtownboy <chr6502@naver.com>
2024-01-16 11:52:22 +0900

    2024.01.16 commit
    1. jwt를 이용한 인증방법 추가 개발

--- src/main/java/com/develop/devlog/config/AuthResolver.java
+++ src/main/java/com/develop/devlog/config/AuthResolver.java
@@ -4,35 +4,77 @@ import com.develop.devlog.config.data.UserSession;
 import com.develop.devlog.domain.Session;
 import com.develop.devlog.exception.Unauthorized;
 import com.develop.devlog.repository.SessionRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
     private final SessionRepository sessionRepository;
+    private static final String KEY = "drCzhdmlM81fPyQDwGEZ476mByd81azLp7jd189LQ2A=";
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.getParameterType().equals(UserSession.class);
     }
+// todo 쿠키를 사용한 방법
+
+//    @Override
+//    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+//        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+//        if(servletRequest == null) {
+//            throw new Unauthorized();
+//        }
+//
+//        Cookie[] cookies = servletRequest.getCookies();
+//        if (cookies.length == 0) {
+//            throw new Unauthorized();
+//        }
+//
+//        String accessToken = cookies[0].getValue();
+//        if (accessToken == null || accessToken.equals("")) {
+//            throw new Unauthorized();
+//        }
+//
+//        Session session = sessionRepository.findByAccessToken(accessToken)
+//                .orElseThrow(Unauthorized::new);
+//
+//        return new UserSession(session.getUser().getId());
+//    }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if (accessToken == null || accessToken.equals("")) {
+        String jws = webRequest.getHeader("Authorization");
+        if (jws == null || jws.equals("")) {
             throw new Unauthorized();
         }
 
-        Session session = sessionRepository.findByAccessToken(accessToken)
-                .orElseThrow(Unauthorized::new);
+        // todo 암호화 관련하여 개선 필요
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(KEY)
+                    .build()
+                    .parseClaimsJws(jws);
 
-        return new UserSession(session.getUser().getId());
+            String userId = claims.getBody().getSubject();
+            return new UserSession(Long.parseLong(userId));
+        } catch (JwtException e) {
+            throw new Unauthorized();
+        }
     }
 }
